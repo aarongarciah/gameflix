6f17fd960fb84ff014fd5933cbc728d8c9dd7d9b
Aarón García Hervás <aar.garc.herv@gmail.com>
2017-03-30 01:07:18 +0200

    Added Gulp workflow and config files.
    Added Gulp workflow.
    Added config files.
    Added favicon.

---
+++ gulpfile.js
@@ -0,0 +1,126 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const cssmin = require('gulp-minify-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const jshint = require('gulp-jshint');
+const scsslint = require('gulp-sass-lint');
+const cache = require('gulp-cached');
+const prefix = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
+const size = require('gulp-size');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const plumber = require('gulp-plumber');
+const babel = require('gulp-babel');
+const notify = require('gulp-notify');
+
+const reload = browserSync.reload;
+
+gulp.task('scss', () => {
+  const onError = (err) => {
+    notify.onError({
+      title: 'Gulp',
+      subtitle: 'Sass error!',
+      message: 'Error: <%= error.message %>',
+      sound: 'Beep',
+    })(err);
+    this.emit('end');
+  };
+
+  return gulp.src('src/scss/main.scss')
+    .pipe(plumber({
+      errorHandler: onError,
+    }))
+    .pipe(sass())
+    .pipe(size({
+      gzip: true,
+      showFiles: true,
+    }))
+    .pipe(prefix({
+      browsers: ['last 2 versions'],
+    }))
+    .pipe(rename('j.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(cssmin())
+    .pipe(size({
+      gzip: true,
+      showFiles: true,
+    }))
+    .pipe(rename({
+      suffix: '.min',
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(reload({
+      stream: true,
+    }));
+});
+
+gulp.task('js', () => {
+  gulp.src('src/js/*.js')
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(size({
+      gzip: true,
+      showFiles: true,
+    }))
+    .pipe(concat('j.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min',
+    }))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(reload({
+      stream: true,
+    }));
+  gulp.src('src/js/nobundle/**/*.js')
+    .pipe(gulp.dest('dist/js/nobundle'));
+});
+
+gulp.task('scss-lint', () => {
+  gulp.src('src/scss/**/*.scss')
+    .pipe(cache('scsslint'))
+    .pipe(scsslint());
+});
+
+gulp.task('jshint', () => {
+  gulp.src('src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('imgmin', () => {
+  gulp.src('src/img/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false,
+      }],
+      use: [pngquant()],
+    }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('browser-sync', () => {
+  browserSync({
+    open: true,
+  });
+});
+
+gulp.task('browser-sync', () => browserSync({
+  server: {
+    baseDir: './',
+  },
+  open: true,
+}));
+
+gulp.task('watch', () => {
+  gulp.watch('src/scss/**/*.scss', ['scss']);
+  gulp.watch('src/js/*.js', ['jshint', 'js']);
+  gulp.watch('src/img/*', ['imgmin']);
+});
+
+gulp.task('default', ['browser-sync', 'js', 'imgmin', 'scss', 'watch']);
